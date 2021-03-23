9c9db0302d34cfefa52bbc7b083631c2b2073aef
Thawatchai <2mercy@outlook.co.th>
2021-03-23 12:02:23 +0700

    LAB10: Update and Delete

--- src/CompanyManagement.java
+++ src/CompanyManagement.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class CompanyManagement {
 
@@ -7,32 +8,130 @@ public class CompanyManagement {
         //Create instance
         EmployeeDAOImpl dao = EmployeeDAOImpl.getInstance();
 
+        //display option list
+        int s = 0;
+        do {
+            System.out.println("Please, select options below: ");
+            System.out.println("1.Display all employee data. ");
+            System.out.println("2.Add new employee. ");
+            System.out.println("3.Find employee by ID. ");
+            System.out.println("4.Update employee data. ");
+            System.out.println("5.Delete employee data. ");
+            System.out.println("6.Exist. ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, select number (4-6): ");
+
+        s = Integer.parseInt(sc.nextLine());
+
+        switch (s) {
+            case 1:
+                displayAllEmp(dao);
+                break;
+            case 2:
+                addNewEmp(dao);
+                break;
+            case 3:
+                findEmpByID(dao);
+                break;
+            case 4:
+                updateEmp(dao);
+                break;
+            case 5:
+                deleteEMP(dao);
+                break;
+            case 6:
+                System.exit(1);
+            default:
+                System.out.println("Please. select only specific number (1-6)!!! ");
+        }
+    } while (s !=0 );
+
         //display all employee data
-        displayAllEmp(dao);
+        //displayAllEmp(dao);
 
         //add new employee
-        addNewEmp(dao);
+        //addNewEmp(dao);
 
         //find employee my id.
-        findEmpByID(dao);
+        //findEmpByID(dao);
+
+        //find employee by id
+        //findEmpByID(dao);
+
+        //update Employee to database
+        //updateEmp(dao);
+
+        //delete employee by id
+        //deleteEMP(dao);
 
     }//class
 
-    private static void findEmpByID(EmployeeDAOImpl dao) {
-        Employee emp = dao.findById("EMP001");
+    private static void deleteEMP(EmployeeDAOImpl dao) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter employee id that you want to delete: ");
+        String s = sc.nextLine();
 
+        dao.deleteEmp(s);
+    }
+
+    private static void updateEmp(EmployeeDAOImpl dao) {
+        //search employee data in database
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an employee id?: ");
+        String id = sc.nextLine();
+
+        Employee emp = dao.findById(id);
+
+        //update
+        System.out.println("Employee info: ");
+        System.out.println(emp.toString());
+
+        //edit position
+        System.out.print("Enter new position?: ");
+        String p = sc.nextLine();
+        emp.setPosition(p);
+
+        //update new data to database
+        dao.updateEmp(emp);
+    }
+
+    private static Employee findEmpByID(EmployeeDAOImpl dao) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter an employee id: ");
+        String id = sc.nextLine();
+
+        Employee emp = dao.findById(id);
         if (emp != null){
             System.out.println(emp.toString());
+        }else {
+            System.out.println(emp.toString());
         }
-
+        return emp;
     }
 
     private static void addNewEmp(EmployeeDAOImpl dao) {
-        Employee myEmp = new Employee("EMP004",
-                "Krittitat Raksa",
-                "Programer",
-                "Krittitat.11@hotmail.com",
-                35000);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter data for new employee: ");
+
+        System.out.print("Employee ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Employee name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Employee position: ");
+        String position = sc.nextLine();
+
+        System.out.print("Employee email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Employee salary: ");
+        double salary = Double.parseDouble(sc.nextLine());
+
+        Employee myEmp = new Employee(id, name, position, email, salary);
         dao.addEmp(myEmp);
     }
 
